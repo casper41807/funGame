e2308a5dc9ba7f76bfb49707d547317c7fa959a7
casper41807 <casper41807@gmail.com>
2022-04-16 13:46:48 +0800

    Initial Commit

---
+++ funGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  funGame
+//
+//  Created by 陳秉軒 on 2022/4/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
